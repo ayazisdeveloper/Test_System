e5556580fbd608180287b2cd0e8be4d18a2efc2e
ahteshamshah <ihtesham.info@gmail.com>
2018-10-25 21:16:05 +0500

    check

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include('header.php');
 
 <?php
 
-echo 'ahtesham shah';
 	if(isset($_REQUEST['page'])){
 			if(file_exists('public/'.$_REQUEST['page'].'.php')){
 				include('public/'.$_REQUEST['page'].'.php');
