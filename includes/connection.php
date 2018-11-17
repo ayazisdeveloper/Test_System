2655a69ccad716a341dc7ad67ca04dbb0ecb5de8
Ahtesham Shah <info.ihtesham3499@gmail.com>
2018-11-17 18:16:29 +0500

    update my project add connection and insert query with oops

--- includes/connection.php
+++ includes/connection.php
@@ -9,6 +9,34 @@ class database{
 			}
 		}
 
+
+	function insert(){
+		
+		if(isset($_REQUEST['register'])){
+			$email = $_REQUEST['user_email'];
+			$password = $_REQUEST['user_password'];
+			$query = mysqli_query($this->conn,"INSERT INTO users(user_email,user_password) VALUES ('$email','$password')");
+			
+			
+			if($query){
+				echo '<h3>'.'data has been inserted'.'</h4>';
+			}
+		}
+		
+	}
+
+	function select(){
+		$select_query = mysqli_query($this->conn,"SELECT * FROM users");
+		$result = mysqli_fetch_array($select_query);
+		while($result = mysqli_fetch_array){
+			echo $result['user_email'].$result['user_password'];
+		}
+	}
+
 }
+
 $call = new database();
+$call->insert();
+$call->select();
+
 ?>
--- public/register.php
+++ public/register.php
@@ -1,25 +1,3 @@
-<?php
-class db{
-	function insert(){
-		
-		if(isset($_REQUEST['register'])){
-			$email = $_REQUEST['user_email'];
-			$password = $_REQUEST['user_password'];
-			$query = mysqli_query($this->Conn,"INSERT INTO users(user_email,user_password) VALUES ('$email','$password')");
-			
-			
-			if($query){
-				echo '<h3>'.'database inserted'.'</h4>';
-			}
-		}
-		
-	}
-}
-$call = new db();
-$call->insert();
-?>
-
-
 <h1>Register</h1>
 <form action="" method="post">
 	<fieldset>
